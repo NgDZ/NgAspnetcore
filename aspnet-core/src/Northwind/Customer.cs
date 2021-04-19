4c114608e0de8f2f28993fe061aaa4426b25a7af
badre429 <badre429@hotmail.com>
2021-04-19 12:50:42 +0100

    dto Custom  implement IEntity

--- aspnet-core/src/Northwind/Customer.cs
+++ aspnet-core/src/Northwind/Customer.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Domain.Entities;
 
 #nullable disable
 
 namespace Northwind
 {
-    public partial class Customer
+    public partial class Customer : IEntity<long>, IEntityDto
     {
+        public object[] GetKeys()
+        {
+            return new object[] { Id };
+        }
         public Customer()
         {
             CustomerCustomerDemographics = new HashSet<CustomerCustomerDemographic>();
