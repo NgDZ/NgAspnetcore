36c2fec8c9825eeeff75ab9463ad59bbcdd25e8d
badre429 <badre429@hotmail.com>
2021-04-20 12:51:48 +0100

    dto CustomDeom implent IEntity

--- aspnet-core/src/Northwind/CustomerCustomerDemo.cs
+++ aspnet-core/src/Northwind/CustomerCustomerDemo.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Domain.Entities;
 
 #nullable disable
 
 namespace Northwind
 {
-    public partial class CustomerCustomerDemo
+    public partial class CustomerCustomerDemo : IEntity<string>, IEntityDto
     {
+        public object[] GetKeys()
+        {
+            return new object[] { Id };
+        }
         public string Id { get; set; }
         public string CustomerTypeId { get; set; }
     }
