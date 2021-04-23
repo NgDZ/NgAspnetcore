7edfea330fb849ed69c0abb6ecc2d2883d05c012
badre429 <badre429@hotmail.com>
2021-04-23 14:03:07 +0100

    CustomerDemographic as IEntity

--- aspnet-core/src/Northwind/CustomerDemographic.cs
+++ aspnet-core/src/Northwind/CustomerDemographic.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Domain.Entities;
 
 #nullable disable
 
 namespace Northwind
 {
-    public partial class CustomerDemographic
+    public partial class CustomerDemographic : IEntity<long>, IEntityDto
     {
+        public object[] GetKeys()
+        {
+            return new object[] { Id };
+        }
         public CustomerDemographic()
         {
             CustomerCustomerDemographics = new HashSet<CustomerCustomerDemographic>();
