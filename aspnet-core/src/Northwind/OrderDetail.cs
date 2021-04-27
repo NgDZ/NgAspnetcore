cb0cfb56c278e883e3b352a7bc6eafb037d24c79
badre429 <badre429@hotmail.com>
2021-04-27 12:54:31 +0100

    OrderDetait as  IEntity

--- aspnet-core/src/Northwind/OrderDetail.cs
+++ aspnet-core/src/Northwind/OrderDetail.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Domain.Entities;
 
 #nullable disable
 
 namespace Northwind
 {
-    public partial class OrderDetail
+    public partial class OrderDetail: IEntity<long>, IEntityDto
     {
+        public object[] GetKeys()
+        {
+            return new object[] { Id };
+        }
         [Column("EntityId")]
         public long Id { get; set; }
         public long OrderId { get; set; }
