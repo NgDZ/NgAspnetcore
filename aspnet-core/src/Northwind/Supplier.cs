26d547de37ba825635765ff95afa3a06a52dc682
badre429 <badre429@hotmail.com>
2021-05-06 10:44:36 +0100

    supplier as IEntity

--- aspnet-core/src/Northwind/Supplier.cs
+++ aspnet-core/src/Northwind/Supplier.cs
@@ -1,19 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Domain.Entities;
 
 #nullable disable
 
 namespace Northwind
 {
-    public partial class Supplier
+    public partial class Supplier : IEntity<long>, IEntityDto
     {
+
+        public object[] GetKeys()
+        {
+            return new object[] { Id };
+        }
         public Supplier()
         {
             Products = new HashSet<Product>();
         }
 
-        [Column("EntityId")] 
+        [Column("EntityId")]
         public long Id { get; set; }
         public string CompanyName { get; set; }
         public string ContactName { get; set; }
