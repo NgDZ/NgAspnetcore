0cae31d274a0a6e331b460af1fa24fe39f0c5f4f
badre429 <badre429@hotmail.com>
2022-01-05 11:13:18 +0100

    fix wip

---
+++ angular/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      blue: "#1fb6ff",
+      purple: "#7e5bef",
+      pink: "#ff49db",
+      orange: "#ff7849",
+      green: "#13ce66",
+      yellow: "#ffc82c",
+      "gray-dark": "#273444",
+      gray: "#8492a6",
+      "gray-light": "#d3dce6",
+    },
+    fontFamily: {
+      sans: ["Roboto", "sans-serif"],
+      serif: ["Roboto", "serif"],
+    },
+    extend: {},
+  },
+};
